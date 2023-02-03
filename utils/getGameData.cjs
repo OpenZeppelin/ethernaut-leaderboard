874fc4d309bb1a106d6dd78c5162ea8124917974
JithinKS <jithinks112@gmail.com>
2023-02-03 16:46:47 +0530

    added axios

--- utils/getGameData.cjs
+++ utils/getGameData.cjs
@@ -1,11 +1,11 @@
 
-
+const axios = require('axios');
 let gameData;
 
 const getGameData = async () => { 
    if (!gameData) {
-        const response = await fetch('https://raw.githubusercontent.com/OpenZeppelin/ethernaut/master/client/src/gamedata/gamedata.json')
-        gameData = await response.json()
+        const response = await axios.get('https://raw.githubusercontent.com/OpenZeppelin/ethernaut/master/client/src/gamedata/gamedata.json')
+        gameData = response.data
     }
     return gameData
 }
